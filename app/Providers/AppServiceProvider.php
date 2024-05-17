8dedd57dab82629878815a345974801693331018
Hoàng Kiên <kienhv@appotapay.com>
2024-05-17 14:16:37 +0700

    update page builder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\HtmlString;
 use Illuminate\Support\ServiceProvider;
+use Z3d0X\FilamentFabricator\Facades\FilamentFabricator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        FilamentFabricator::favicon(asset('assets/image/logo.png'));
+        FilamentFabricator::pushMeta([
+            new HtmlString('<meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <meta name="robots" content="index, follow">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="title" content="Trà Thái Việt">
+            <meta name="description" content="Trà Thái Việt, tinh hoa trà Việt">
+            <meta name="keywords" content="trà, thái việt, trà xanh, trà cụ, trà đinh, trà nõn">
+            <meta name="language" content="Vietnamese">'),
+        ]);
     }
 }
